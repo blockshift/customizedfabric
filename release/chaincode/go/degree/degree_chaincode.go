782d664c7ba1f54961fa8dfb598d2551185e3712
MohammadObaid <mobaid56@gmail.com>
2017-05-29 15:26:35 +0500

    invokedegreecustomization

--- release/chaincode/go/degree/degree_chaincode.go
+++ release/chaincode/go/degree/degree_chaincode.go
@@ -90,12 +90,12 @@ import (
 type SimpleChaincode struct {
 }
 
-type marble struct {
+type degree struct {
 	ObjectType string `json:"docType"` //docType is used to distinguish the various types of objects in state database
 	Degreeholder       string `json:"degreeholder"`    //the fieldtags are needed to keep case from bouncing around
 	Department         string `json:"department"`
-	Batch              string `json:"batch"`
-	Gpa                string `json:"gpa"`
+	Enrollmentnumber   string `json:"batch"`
+        Gpa                string `json:"gpa"`
         University         string `json:"university"`  
 }
 
@@ -141,19 +141,19 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 // ============================================================
-// initMarble - create a new marble, store into chaincode state
+// initDegree - create a new degree, store into chaincode state
 // ============================================================
 func (t *SimpleChaincode) initDegree(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var err error
 
-	//   0       1       2     3
-	// "asdf", "blue", "35", "bob"
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
+	//   0          1              2            3           4
+	// "Obaid", "Software", "Ned/1186/13-14", "3.5" , "Ned University"
+	if len(args) != 5 {
+		return shim.Error("Incorrect number of arguments. Expecting 5")
 	}
 
 	// ==== Input sanitation ====
-	fmt.Println("- start init marble")
+	fmt.Println("- start init Degree")
 	if len(args[0]) <= 0 {
 		return shim.Error("1st argument must be a non-empty string")
 	}
@@ -166,57 +166,48 @@ func (t *SimpleChaincode) initDegree(stub shim.ChaincodeStubInterface, args []st
 	if len(args[3]) <= 0 {
 		return shim.Error("4th argument must be a non-empty string")
 	}
-	marbleName := args[0]
-	color := strings.ToLower(args[1])
-	owner := strings.ToLower(args[3])
-	size, err := strconv.Atoi(args[2])
+        if len(args[4]<=0){
+                return shim.Error("5th Argument must be non-empty string")
+        } 
+	Degreeholder     := args[0]
+	Department       := args[1]
+	Enrollmentnumber := args[2]
+	Gpa              := args[3]
+        University       := args[4]  
+	
+
+	// ==== Check if degree already exists ====
+	degreeAsBytes, err := stub.GetState(Enrollmentnumber)
 	if err != nil {
-		return shim.Error("3rd argument must be a numeric string")
+		return shim.Error("No  record found: " + err.Error())
+	} else if degreeAsBytes != nil {
+		fmt.Println("This degree already exists with enrolment number: " + Enrollmentnumber)
+		return shim.Error("This degree already exists with enrolment number: " + Enrollmentnumber)
 	}
 
-	// ==== Check if marble already exists ====
-	marbleAsBytes, err := stub.GetState(marbleName)
-	if err != nil {
-		return shim.Error("Failed to get marble: " + err.Error())
-	} else if marbleAsBytes != nil {
-		fmt.Println("This marble already exists: " + marbleName)
-		return shim.Error("This marble already exists: " + marbleName)
-	}
-
-	// ==== Create marble object and marshal to JSON ====
-	objectType := "marble"
-	marble := &marble{objectType, marbleName, color, size, owner}
-	marbleJSONasBytes, err := json.Marshal(marble)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	//Alternatively, build the marble json string manually if you don't want to use struct marshalling
-	//marbleJSONasString := `{"docType":"Marble",  "name": "` + marbleName + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + `, "owner": "` + owner + `"}`
-	//marbleJSONasBytes := []byte(str)
-
-	// === Save marble to state ===
-	err = stub.PutState(marbleName, marbleJSONasBytes)
+	// ==== Create degree object and marshal to JSON ====
+	objectType := "degree"
+	degree := &degree{objectType,Degreeholder,Department,Enrollmentnumber,Gpa,University }
+	degreeJSONasBytes, err := json.Marshal(degree)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-
-	//  ==== Index the marble to enable color-based range queries, e.g. return all blue marbles ====
-	//  An 'index' is a normal key/value entry in state.
-	//  The key is a composite key, with the elements that you want to range query on listed first.
-	//  In our case, the composite key is based on indexName~color~name.
-	//  This will enable very efficient state range queries based on composite keys matching indexName~color~*
-	indexName := "color~name"
-	colorNameIndexKey, err := stub.CreateCompositeKey(indexName, []string{marble.Color, marble.Name})
+	// === Save degree to state . Everything is stored in bytes. So we need to marshell it and at the time of receving we need to unmarhall everything so it becomes readable ===
+	err = stub.PutState(Enrollmentnumber, degreeJSONasBytes)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	//  Save index entry to state. Only the key name is needed, no need to store a duplicate copy of the marble.
-	//  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
-	value := []byte{0x00}
-	stub.PutState(colorNameIndexKey, value)
+// Index degree with respect to degree holder and enrolment and create composite key of both field
+	indexName := "degreeholderenrol"
+	holerEnrolIndexKey, err := stub.CreateCompositeKey(indexName, []string{degree.Degreeholder, marble.Degreeholder})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+		value := []byte{0x00}
+	stub.PutState(holerEnrolIndexKey, value)
 
-	// ==== Marble saved and indexed. Return success ====
-	fmt.Println("- end init marble")
+	// ==== Degree saved and indexed. Return success ====
+	fmt.Println("- end init degree")
 	return shim.Success(nil)
 }
 
